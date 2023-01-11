42555fafee1abbe3d5367607ead21e97cf6d4bb4
KrishnaTangirala1993 <krishna.tvsr@gmail.com>
2023-01-11 20:15:39 +0530

    Re-Code

--- src/App.js
+++ src/App.js
@@ -1,48 +1,13 @@
 import React from 'react';
 import Navbar from "./components/Navbar";
 import image from "./components/kunda.jpg";
-import { Route, Routes, Switch} from "react-router-dom";
-import About from "./components/About";
-import Appointment from './components/Appointment';
-import Contact from './components/Contact';
-import Home from './components/Home';
-import Specialities from './components/Specialities';
+
 
  function App() {
-  let component
-  switch (window.location.pathname) {
-    case "/":
-      component = <App />
-      break;
-  case "/about":
-    component = <About />
-    break;
-    case "/appointment":
-    component = <Appointment />
-    break;
-
-    case "/contact":
-    component = <Contact />
-    break;
-    case "/home":
-    component = <Home />
-    break;
-
-    case "/specialities":
-    component = <Specialities />
-    break;
- }
-
+  
   return (
-    <><><div style={{ backgroundImage: `url(${image})` }} /><Navbar />
-    {component}
-    </>
+    <><div style={{ backgroundImage: `url(${image})` }} /><Navbar /></>
     
-    {/* <Routes>
-     
-     <Route  path="/components/About" component={About}/>
-    </Routes></> */}
-     </>
   );
 };
 
